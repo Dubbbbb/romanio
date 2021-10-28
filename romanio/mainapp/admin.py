f4e96090eca8ce6a9beae6aa406569d8129809f9
Vlad Dubatovka <adravel2363@gmail.com>
2021-10-28 17:13:19 +0300

    start model

--- romanio/mainapp/admin.py
+++ romanio/mainapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class AdminDoor(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Door,AdminDoor)
--- romanio/mainapp/models.py
+++ romanio/mainapp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    class Meta:
+        abstract = True
+
+    category = models.ForeignKey("Category", verbose_name="Категория", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name="Изображение")
+    description = models.TextField(verbose_name="Описание", null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Цена")
+
+
+    def __str__(self):
+        return self.title
+
+class Door(Product):
+
+    class Meta():
+        verbose_name = 'Дверь'
+        verbose_name_plural = 'Двери'
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Категория")
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta():
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категория'
+
+
+
