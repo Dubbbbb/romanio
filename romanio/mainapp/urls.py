5dc406b811119904613c549eb30e05202ea60cd6
Vladislav Urievich <vladik040599@gmail.com>
2021-11-02 22:25:36 +0300

    форму блять делаем нахуй

--- romanio/mainapp/urls.py
+++ romanio/mainapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', HomePage.as_view(), name="home"),
     # path('contacts/', contacts_page, name="contacts_page"),
     path('catalog/<str:slug_url>/',CatalogPage.as_view(), name="category"),
-    path('<str:slug_category>/<str:slug_url>/', ProductDetail.as_view(), name="product_detail")
+    path('catalog/<str:slug_category>/<str:slug_url>/', ProductDetail.as_view(), name="product_detail")
 ]
--- romanio/user/urls.py
+++ romanio/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import UserSignIn
 
 urlpatterns = [
     path('signin/', UserSignIn.as_view(), name="signin"),
